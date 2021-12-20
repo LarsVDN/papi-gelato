076cd19380b3fc0ab1fc9786693254fbc28034fd
LarsVDN <20481220+LarsVDN@users.noreply.github.com>
2021-12-20 12:48:01 +0100

    made some tiny edits n stuff

--- papi-gelato.py
+++ papi-gelato.py
@@ -26,29 +26,62 @@
 #
 #
 
-max = 8
+
 
 def error():
     print("Sorry dat snap ik niet...")
 
-def start(): # Stap 1
-    try:
-        aantal = int(input("Hoeveel bolletjes wilt u? "))
-        if aantal <= 3:
-            bakHoorn()
-        elif aantal >= 4 and aantal <= max:
-            laatsteStap()
-        elif aantal > max:
-            print("Sorry, zulke grote bakken hebben we niet")
-            start()
-    except ValueError:
-        print("U mag alleen cijfers invoeren")
-        start()
-def bakHoorn(): # Stap 2
-    pass
+def vraagBolletjes():
+    aantalok = False
+    while not aantalok:
+        try:
+            aantal = int(input("Hoeveel bolletjes wilt u? "))
+            aantalok = True
+        except ValueError:
+            print("U mag alleen cijfers invoeren")
+    return aantal
 
-def laatsteStap(): # Stap 3
-    pass
+
+def vraagBakHoorn(aantal):
+    a = 'hoorntje'
+    b = 'bakje'
+    max = 8
+
+    if aantal <=4:
+        while True:
+            vraagHB = input("Wilt u deze " + str(aantal) + " bolletje(s) in een hoorntje of een bakje? ")
+
+            if vraagHB in [a,b]:
+                return vraagHB
+            else:
+                print("U mag alleen 'hoorntje' of 'bakje' invoeren")
+    elif aantal >=4 and aantal <=max:
+
+        print("Dan krijgt u van mij een " + str(b) + " met " + str(aantal) + " bolletjes")
+    elif aantal > max:
+        print("Sorry, zulke grote bakken hebben wij niet")
+
+
+
+def main(): # Stap 1
+    bolletjes = vraagBolletjes()
+    bakHoorntje = vraagBakHoorn(bolletjes)
 
 print("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.")
-start()
+main()
+
+
+
+
+
+
+
+
+
+# if aantal <= 3:
+#     bakHoorn()
+# elif aantal >= 4 and aantal <= max:
+#     laatsteStap()
+# elif aantal > max:
+#     print("Sorry, zulke grote bakken hebben we niet")
+#     start()
